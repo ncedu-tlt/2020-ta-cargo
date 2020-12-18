2bc0324212926f05b1717e42c5a80fdcfe1f85ef
b.marchenko <Dontapproachmei@mail.ru>
2020-12-18 17:01:59 +0400

    Добавил контроллер, сервис

--- src/main/java/com/netcracker/controller/OrderController.java
+++ src/main/java/com/netcracker/controller/OrderController.java
@@ -1,16 +1,60 @@
 package com.netcracker.controller;
+import com.netcracker.model.Order;
+import com.netcracker.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 public class OrderController {
 
-    @GetMapping("/orderID")
-    public String hello(@RequestParam(value = "orderID", defaultValue = "?") String name) {
-        return String.format("Hello %s!", name);
+    private final OrderService orderService;
+
+    @Autowired
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
     }
+
+    @PostMapping("/order")
+    public ResponseEntity<?> create(@RequestBody Order order){
+        orderService.create(order);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("order")
+    public ResponseEntity<List<Order>>readeAll(){
+        final List<Order> orderList = orderService.readAll();
+        return orderList != null && !orderList.isEmpty()
+                ? new ResponseEntity<>(orderList, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/order/{id}")
+    public ResponseEntity<Order> readId(@PathVariable(name = "id") int id){
+        final Order order = orderService.read(id);
+        return order != null
+                ? new ResponseEntity<>(order, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("/order/{id}")
+    public ResponseEntity<?> update(@PathVariable(name = "id") int id,
+                                    @RequestBody Order order){
+        final  boolean update = orderService.update(id, order);
+        return update
+                ? new ResponseEntity<>(HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
+    }
+
+    @DeleteMapping("/order/{id}")
+    public ResponseEntity<?> delete(@PathVariable(name = "id") int id){
+        boolean delete = orderService.delete(id);
+        return delete
+                ? new ResponseEntity<>(HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
+    }
+
 }
---
+++ src/main/java/com/netcracker/service/OrderService.java
@@ -0,0 +1,54 @@
+package com.netcracker.service;
+
+import com.netcracker.model.Order;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Service
+public class OrderService implements Serviceable<Order>{
+
+    private static final Map<Integer, Order> ORDER_MAP = new HashMap<>();
+    private static final AtomicInteger ORDER_ID_HOLDER = new AtomicInteger();
+
+
+    @Override
+    public void create(Order order) {
+        final int id = ORDER_ID_HOLDER.incrementAndGet();
+        order.setOrderId(id);
+        ORDER_MAP.put(id,order);
+    }
+
+    @Override
+    public List<Order> readAll() {
+        List<Order> orderList = new ArrayList<>(ORDER_MAP.values());
+        return orderList;
+    }
+
+    @Override
+    public Order read(int id) {
+        Order order = ORDER_MAP.get(id);
+        return order;
+    }
+
+    @Override
+    public boolean update(int id, Order object) {
+        if (ORDER_MAP.containsKey(id)) {
+            object.setOrderId(id);
+            ORDER_MAP.put(id, object);
+            return true;
+        } else return false;
+    }
+
+    @Override
+    public boolean delete(int id) {
+        if (ORDER_MAP.containsKey(id)) {
+            ORDER_MAP.remove(id);
+            return true;
+        }else return false;
+    }
+}
