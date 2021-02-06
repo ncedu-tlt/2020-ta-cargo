f85447fe6852eda22c5a3739ce8d8285b70abac6
OlegTselishchev <oleks63@mail.ru>
2021-02-06 18:38:07 +0400

    create ClientRepository

---
+++ src/main/java/com/netcracker/repository/ClientRepository.java
@@ -0,0 +1,16 @@
+package com.netcracker.repository;
+
+import com.netcracker.model.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client,Integer> {
+
+    Optional<Client> findById(Integer id);
+    Optional<Client> findByEmail(String email);
+    Optional<Client> findByPhone(String phone);
+
+}
