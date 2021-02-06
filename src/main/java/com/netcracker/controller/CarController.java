c7f053bb20e0f455848b3faeb6e7df36042a0b7f
OlegTselishchev <oleks63@mail.ru>
2021-02-06 16:28:29 +0400

    added displayAll

--- src/main/java/com/netcracker/controller/CarController.java
+++ src/main/java/com/netcracker/controller/CarController.java
@@ -1,8 +1,14 @@
 package com.netcracker.controller;
 
+import com.netcracker.model.Car;
 import com.netcracker.service.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CarController {
@@ -14,4 +20,11 @@ public class CarController {
         this.carService = carService;
     }
 
+    @GetMapping("/car")
+    public ResponseEntity<List<Car>> displayAll(){
+        final List<Car> carList = carService.displayAll();
+        return carList != null && !carList.isEmpty()
+                ? new ResponseEntity<>(carList, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
 }
