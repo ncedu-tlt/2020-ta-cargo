34e4a6d9200df664034a40b2b91183b962154abb
OlegTselishchev <oleks63@mail.ru>
2021-02-06 15:59:59 +0400

    updated gitignore, application.properties, pom.xml, Client, Address

--- src/main/java/com/netcracker/model/Client.java
+++ src/main/java/com/netcracker/model/Client.java
@@ -1,12 +1,26 @@
 package com.netcracker.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cg_clients")
 public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Integer userId;
+    @Column(name = "lastname")
     private String lastName;
+    @Column(name = "firstname")
     private String firstName;
+    @Column(name = "middlename")
     private String middleName;
+    @Column(name = "phone")
     private String phone;
+    @Column(name = "email")
     private String email;
+    @Column(name = "drive_category")
     private String driveCategory;
 
     public Integer getUserId() {
@@ -14,7 +28,7 @@ public class Client {
     }
 
     public void setUserId(Integer userId) {
-        if (userId != null)  {
+        if (userId != null) {
             this.userId = userId;
         }
     }
@@ -24,7 +38,7 @@ public class Client {
     }
 
     public void setLastName(String lastName) {
-        if ((lastName != null)&&(!lastName.isEmpty())) {
+        if ((lastName != null) && (!lastName.isEmpty())) {
             this.lastName = lastName;
         }
     }
@@ -34,48 +48,8 @@ public class Client {
     }
 
     public void setFirstName(String firstName) {
-        if ((firstName != null)&&(!firstName.isEmpty())) {
+        if ((firstName != null) && (!firstName.isEmpty())) {
             this.firstName = firstName;
-        }
-    }
-
-    public String getMiddleName() {
-        return middleName;
-    }
-
-    public void setMiddleName(String middleName) {
-        if ((middleName != null)&&(!middleName.isEmpty())) {
-            this.middleName = middleName;
-        }
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        if ((phone != null)&&(!phone.isEmpty())) {
-            this.phone = phone;
-        }
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        if ((email != null)&&(!email.isEmpty())) {
-            this.email = email;
-        }
-    }
-
-    public String getDriveCategory() {
-        return driveCategory;
-    }
-
-    public void setDriveCategory(String driveCategory) {
-        if ((driveCategory != null)&&(!driveCategory.isEmpty())) {
-            this.driveCategory = driveCategory;
         }
     }
 }
