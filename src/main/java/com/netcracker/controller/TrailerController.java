50507ea38515248b7ae99cc118e83cdcb22c2f3b
OlegTselishchev <oleks63@mail.ru>
2021-02-06 19:29:34 +0400

    added Trailer Create

--- src/main/java/com/netcracker/controller/TrailerController.java
+++ src/main/java/com/netcracker/controller/TrailerController.java
@@ -1,10 +1,11 @@
 package com.netcracker.controller;
 
+import com.netcracker.model.Trailer;
 import com.netcracker.service.TrailerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-
 
 @RestController
 public class TrailerController {
@@ -16,4 +17,9 @@ public class TrailerController {
         this.trailerService = trailerService;
     }
 
+    @PostMapping("/trailer")
+    public ResponseEntity<?> create(@RequestBody Trailer trailer){
+        trailerService.create(trailer);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
 }
