f1d6763cc9deec4a16c6510379cce36cfbb758b7
OlegTselishchev <oleks63@mail.ru>
2021-02-06 16:14:59 +0400

    added CarController

---
+++ src/main/java/com/netcracker/controller/CarController.java
@@ -0,0 +1,17 @@
+package com.netcracker.controller;
+
+import com.netcracker.service.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CarController {
+
+    private final CarService carService;
+
+    @Autowired
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
+}
