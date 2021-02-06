b8725aadf1e41ea7fe4f6577a8970568758ac76a
OlegTselishchev <oleks63@mail.ru>
2021-02-06 16:25:07 +0400

    added delete

--- src/main/java/com/netcracker/controller/CarController.java
+++ src/main/java/com/netcracker/controller/CarController.java
@@ -2,6 +2,10 @@ package com.netcracker.controller;
 
 import com.netcracker.service.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,4 +18,11 @@ public class CarController {
         this.carService = carService;
     }
 
+    @DeleteMapping("/car/{id}")
+    public ResponseEntity<?> delete(@PathVariable(name = "id") int id){
+        boolean delete = carService.delete(id);
+        return delete
+                ? new ResponseEntity<>(HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
+    }
 }
