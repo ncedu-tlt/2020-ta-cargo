275a9a81d053d977c3310cd82efdfce550c30c9f
OlegTselishchev <oleks63@mail.ru>
2021-02-06 19:50:45 +0400

    added Address

--- src/main/java/com/netcracker/controller/AddressController.java
+++ src/main/java/com/netcracker/controller/AddressController.java
@@ -1,13 +1,8 @@
 package com.netcracker.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import com.netcracker.model.Address;
 import com.netcracker.service.AddressService;
-
-import java.util.List;
 
 @RestController
 public class AddressController {
@@ -17,45 +12,6 @@ public class AddressController {
     @Autowired
     public AddressController(AddressService addressService) {
         this.addressService = addressService;
-    }
-
-    @PostMapping("/address")
-    public ResponseEntity<?> create(@RequestBody Address address){
-        addressService.create(address);
-        return new ResponseEntity<>(HttpStatus.CREATED);
-    }
-
-    @GetMapping("/address")
-    public ResponseEntity<List<Address>>readeAll(){
-        final List<Address> clientList = addressService.readAll();
-        return clientList != null && !clientList.isEmpty()
-                ? new ResponseEntity<>(clientList, HttpStatus.OK)
-                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    }
-
-    @GetMapping("/address/{id}")
-    public ResponseEntity<Address> readId(@PathVariable(name = "id") int id){
-        final Address address = addressService.read(id);
-        return address != null
-                ? new ResponseEntity<>(address, HttpStatus.OK)
-                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    }
-
-    @PutMapping("/address/{id}")
-    public ResponseEntity<?> update(@PathVariable(name = "id") int id,
-                                    @RequestBody Address address){
-        final  boolean update = addressService.update(address);
-        return update
-                ? new ResponseEntity<>(HttpStatus.OK)
-                : new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
-    }
-
-    @DeleteMapping("/address/{id}")
-    public ResponseEntity<?> delete(@PathVariable(name = "id") int id){
-        boolean delete = addressService.delete(id);
-        return delete
-                ? new ResponseEntity<>(HttpStatus.OK)
-                : new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
     }
 
 }
