99772b77471d6d5277d68e47dad8d0549f51f818
OlegTselishchev <oleks63@mail.ru>
2021-02-06 19:58:31 +0400

    added Address DisplayAll

--- src/main/java/com/netcracker/controller/AddressController.java
+++ src/main/java/com/netcracker/controller/AddressController.java
@@ -7,6 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.netcracker.service.AddressService;
 
+import java.util.List;
+
 @RestController
 public class AddressController {
 
@@ -29,5 +31,13 @@ public class AddressController {
         return delete
                 ? new ResponseEntity<>(HttpStatus.OK)
                 : new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
+    }
+
+    @GetMapping("/address")
+    public ResponseEntity<List<Address>> displayAll(){
+        final List<Address> addressList = addressService.displayAll();
+        return addressList != null && !addressList.isEmpty()
+                ? new ResponseEntity<>(addressList, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
