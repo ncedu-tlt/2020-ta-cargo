472013d171fcf99a8c4d8dbc9ffe49f0c83a8a3c
OlegTselishchev <oleks63@mail.ru>
2021-02-06 16:22:10 +0400

    added modify

--- src/main/java/com/netcracker/controller/CarController.java
+++ src/main/java/com/netcracker/controller/CarController.java
@@ -1,7 +1,12 @@
 package com.netcracker.controller;
 
+import com.netcracker.model.Car;
 import com.netcracker.service.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,4 +19,11 @@ public class CarController {
         this.carService = carService;
     }
 
+    @PatchMapping("/car")
+    public ResponseEntity<?> modify(@RequestBody Car car ){
+        final boolean updateField = carService.modify(car);
+        return updateField
+                ? new ResponseEntity<>(HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
+    }
 }
