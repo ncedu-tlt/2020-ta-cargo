276bd5b2ad98c1815cd06fd7ccc53992c115e8dd
FriedrichBlack <vigruksik@gmail.com>
2021-02-10 15:42:40 +0400

    displayAll

--- src/main/java/com/netcracker/repository/BoxRepository.java
+++ src/main/java/com/netcracker/repository/BoxRepository.java
@@ -4,7 +4,7 @@ import com.netcracker.model.Box;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface BoxRepository extends JpaRepository<Box, Long> {
+public interface BoxRepository extends JpaRepository<Box, Integer> {
 
 
 }
