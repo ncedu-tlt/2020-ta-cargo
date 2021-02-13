cd0777a404fff870257de8ab157a7473c2d67d90
FriedrichBlack <vigruksik@gmail.com>
2021-02-14 01:38:44 +0400

    delete was created

--- src/main/java/com/netcracker/controller/TypeCargoController.java
+++ src/main/java/com/netcracker/controller/TypeCargoController.java
@@ -2,6 +2,10 @@ package com.netcracker.controller;
 
 import com.netcracker.service.TypeCargoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,5 +17,13 @@ public class TypeCargoController {
 
     public TypeCargoController(TypeCargoService typeService) {
         this.typeService = typeService;
+    }
+
+    @DeleteMapping ("/type/delete/{id}")
+    public ResponseEntity<?> delete (@PathVariable(name = "id") int id){
+        boolean delete = typeService.delete(id);
+        return delete
+                ? new ResponseEntity<>(HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
     }
 }
--- src/main/java/com/netcracker/service/TypeCargoService.java
+++ src/main/java/com/netcracker/service/TypeCargoService.java
@@ -1,12 +1,15 @@
 package com.netcracker.service;
 
 
+import com.netcracker.model.TypeCargo;
 import com.netcracker.repository.TypeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class TypeCargoService {
+public class TypeCargoService implements Serviceable<TypeCargo>{
 
     @Autowired
     private final TypeRepository typeRepository;
@@ -14,5 +17,23 @@ public class TypeCargoService {
 
     public TypeCargoService(TypeRepository typeRepository) {
         this.typeRepository = typeRepository;
+    }
+
+    @Override
+    public void create(TypeCargo object) {
+
+    }
+
+    @Override
+    public List<TypeCargo> displayAll() {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Integer id) {
+        if(typeRepository.existsById(id)){
+            typeRepository.deleteById(id);
+            return true;
+        }else return false;
     }
 }
