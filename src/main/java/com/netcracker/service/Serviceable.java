676fc3207047ea19955af9dc85edecd8cb9dd3cc
FriedrichBlack <vigruksik@gmail.com>
2021-02-14 02:09:55 +0400

    Serviceable

--- src/main/java/com/netcracker/service/Serviceable.java
+++ src/main/java/com/netcracker/service/Serviceable.java
@@ -3,14 +3,9 @@ package com.netcracker.service;
 import java.util.List;
 
 public interface Serviceable<T> {
-
-
     void create(T object);
     List<T> displayAll();
     boolean delete(Integer id);
-    // boolean modify(T ob);
-    // T displayById(Integer id);
-    // boolean update(T object);
 }
 
 
