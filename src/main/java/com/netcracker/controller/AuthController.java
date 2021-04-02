db388c652216ec5824f2d7656b2b6c47e16de01a
OlegTselishchev <oleks63@mail.ru>
2021-04-02 15:39:52 +0400

    add security

--- src/main/java/com/netcracker/controller/AuthController.java
+++ src/main/java/com/netcracker/controller/AuthController.java
@@ -12,10 +12,8 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 import java.util.HashMap;
 import java.util.Map;
