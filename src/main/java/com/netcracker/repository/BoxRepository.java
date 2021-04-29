6fbd33e13b9afb701502c0f97c0c79ab329218b4
OlegTselishchev <oleks63@mail.ru>
2021-04-30 00:14:30 +0400

    update table

--- src/main/java/com/netcracker/repository/BoxRepository.java
+++ src/main/java/com/netcracker/repository/BoxRepository.java
@@ -10,5 +10,5 @@ import java.util.Optional;
 
 @Repository
 public interface BoxRepository extends JpaRepository<Box, Integer> {
-    Optional<List<Box>> findBoxByClientUserId(Integer id);
+   Optional<List<Box>> findBoxByClientUserId(Integer id);
 }
--- src/main/java/com/netcracker/repository/OrderRepository.java
+++ src/main/java/com/netcracker/repository/OrderRepository.java
@@ -2,7 +2,10 @@ package com.netcracker.repository;
 
 
 import com.netcracker.model.Order;
+import com.netcracker.model.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,4 +18,17 @@ public interface OrderRepository extends JpaRepository<Order, Integer> {
 
     List<Order> findOrderByReceiverUserId (Integer id);
 
+    List<Order> findOrderByBoxClientUserId(Integer id);
+
+    List<Order> findOrderByStatusName(String name);
+
+    @Query("select o from Order o where o.box.client.userId = :id and o.status.name = :status")
+    List<Order> findOrderByBoxClientIdAndStatus(@Param("id") Integer id, @Param("status") String status);
+
+    @Query("select o from Order o where o.box.client.userId = :id and o.status.name <> :status")
+    List<Order> findOrderByBoxClientIdAndNotStatus(@Param("id") Integer id, @Param("status") String status);
+
+    @Query("select o from Order o where o.driver.userId = :id and o.status.name = :status")
+    List<Order> findOrderByDriverIdAndStatus(@Param("id") Integer id, @Param("status") String status);
 }
+
